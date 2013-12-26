247ade04878fb4e18964a9a3450b8ea1407030fb
James B. Pollack <jamesbradenpollack@gmail.com>
2013-12-26 11:36:49 -0800

    drumcode!

--- public/templates/templates.js
+++ public/templates/templates.js
@@ -8,11 +8,15 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += " \n      <div class=\"col-lg-3 col-sm-4 col-xs-6\">\n        <a href=\"";
+  buffer += " \n      ";
+  if (stack1 = helpers.debug) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.debug); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\n      <div class=\"col-lg-3 col-sm-4 col-xs-6\">\n        <a href=\"";
   if (stack1 = helpers.link) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.link); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\" target=\"_blank\" class=\"thumbnail\">\n         <img src=\""
+    + "\" class=\"thumbnail\" target=\"_blank\">\n         <img src=\""
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.images)),stack1 == null || stack1 === false ? stack1 : stack1.standard_resolution)),stack1 == null || stack1 === false ? stack1 : stack1.url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" class=\"img-responsive\">\n       </a>\n     </div>\n      ";
   return buffer;
